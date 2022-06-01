f60ff0dbfb9c8448c00d39ae4cfde38d14c5c5cc
Alba Nayeli Blas García <92959210+nayeblas98@users.noreply.github.com>
2022-05-31 21:32:03 -0500

    Add files via upload

---
+++ actividad_1.js
@@ -0,0 +1,26 @@
+//Multiplicación//
+let valor1 = prompt("Ingrese un número");
+
+console.log(valor1 * 2)
+
+//Resta de números//
+var number1 = prompt("Ingrese un número");
+var number2 = prompt("Ingrese otro número");
+
+console.log(number1 - number2)
+
+//área de un círculo//
+const r = 5.3
+const pi = Math.PI
+
+var areaCirculo = (pi*r**2)
+
+console.log("El área del círculo es " + areaCirculo)
+
+//área de un rombo//
+var D = 8
+var d = 6
+
+var areaRombo = D*d/2
+
+console.log("El área del rombo es " + areaRombo)
